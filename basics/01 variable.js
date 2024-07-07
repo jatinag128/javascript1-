f1d0b8ccf2f6e4ccbcd4296fd2aa3844be0d61c1
jatinag128 <144659645+jatinag128@users.noreply.github.com>
2024-07-07 10:48:51 +0530

    Update 01 variable.js

--- basics/01 variable.js
+++ basics/01 variable.js
@@ -1,1 +1,9 @@
-const ac
+const accountId=144553
+let accountEmail="jatin@gmail.com"
+var accountPassword=""12345"
+let accountCity="jaipur"
+let accountState;
+Email="dhruv@gmail.com"
+ accountPassword=""345"
+ accountCity="jaipur"
+ console.table([accountId,accountEmail,accountPassword,accountCity]);
